bd833d9d43bb7db547e66e8d6939d76c04a3efa9
Terry Hearst <thearst3rd@gmail.com>
2021-09-19 11:53:22 -0400

    Read from process.env.PORT

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express")
 const app = express()
 const path = require("path")
 
-const port = 8000
+const port = process.env.PORT || 8000
 
 app.use(express.static("public"))
 
