b1021de7db350d4f448ac750ac149151406d3229
gbtami <gbtami@gmail.com>
2022-10-01 20:31:28 +0200

    Fix alignment of board background

--- generate_css.sh
+++ generate_css.sh
@@ -2,14 +2,13 @@
 # generate SVGs
 for i in {1..12};do for j in {1..10}; do sed "s#0 0 1000 1000#0 $(( 1000 - 100*j )) $(( 100*i )) $(( 100*j ))#" public/assets/images/board/blue_base.svg > "public/assets/images/board/blue${i}x${j}.svg";done;done
 # generate background CSS
-for i in {1..12};do for j in {1..10}; do echo -e ".blue.board${i}x${j} .cg-wrap {\n  background-image: url('images/board/blue${i}x${j}.svg');\n}";done;done > public/assets/theme-backgrounds.css
+for i in {1..12};do for j in {1..10}; do echo -e ".blue.board${i}x${j} cg-board {\n  background-image: url('images/board/blue${i}x${j}.svg');\n}";done;done > public/assets/theme-backgrounds.css
 
 # generate CSS
 rm public/assets/generated.css
 for i in {1..12};do
     for j in {1..10};do
         echo -e ".board${i}x${j} .cg-wrap {\n  width: $(( i > j ? 640 : 640 * i / j ))px;\n  height: $(( j > i ? 640 : 640 * j / i ))px;\n}" >> public/assets/generated.css
-        echo -e ".board${i}x${j} cg-container {\n  width: $(( 100 * i ))%;\n  height: $(( i > 8 ? 800 * j / i : 100 * j ))%;\n}" >> public/assets/generated.css
         echo -e ".board${i}x${j} cg-helper {\n  width: $(echo "scale=5;100 / ${i}" | bc)%;\n  height: $(echo "scale=5;100 / ${j}" | bc)%;\n}" >> public/assets/generated.css
         echo -e ".board${i}x${j} cg-board square {\n  width: $(echo "scale=5;100 / ${i}" | bc)%;\n  height: $(echo "scale=5;100 / ${j}" | bc)%;\n}" >> public/assets/generated.css
         echo -e ".board${i}x${j} .cg-wrap piece {\n  width: $(echo "scale=5;100 / ${i}" | bc)%;\n  height: $(echo "scale=5;100 / ${j}" | bc)%;\n}" >> public/assets/generated.css
