7f7e4a2e61004b4a63f4a8e4f8c5750786c49183
gbtami <gbtami@gmail.com>
2022-10-02 09:12:55 +0200

    Letter piece theme

--- generate_css.sh
+++ generate_css.sh
@@ -4,6 +4,19 @@ for i in {1..12};do for j in {1..10}; do sed "s#0 0 1200 1000#0 $(( 1000 - 100*j
 # generate background CSS
 for i in {1..12};do for j in {1..10}; do echo -e ".blue.board${i}x${j} cg-board {\n  background-image: url('images/board/blue${i}x${j}.svg');\n}";done;done > public/assets/theme-backgrounds.css
 
+# generate theme-letters CSS
+for i in "white" "black";do
+    for j in {a..z};do
+        if [[ $i = "white" ]]
+        then
+           letter=${j^}
+        else
+           letter=${j}
+        fi
+        echo -e ".letters .cg-wrap piece.${j}-piece.${i} {background-image: url(\"data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' version='1.1' width='80px' height='80px'><text font-size='30' x='50%' y='50%' fill='${i}' class='$i' text-anchor='middle' dominant-baseline='central'>${letter}</text></svg>\");}" >> public/assets/theme-letters.css
+    done
+done
+
 # generate CSS
 rm public/assets/generated.css
 for i in {1..12};do
