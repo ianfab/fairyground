df569494616dac6e59de6420c1a1f98a35ab938e
Fabian Fichter <ianfab@users.noreply.github.com>
2023-08-26 16:54:59 +0200

    Reproduce generated SVGs and CSS

--- generate_css.sh
+++ generate_css.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-# generate SVGs
-for i in {1..12};do for j in {1..10}; do sed "s#0 0 1200 1000#0 $(( 1000 - 100*j )) $(( 100*i )) $(( 100*j ))#" public/assets/images/board/blue_base.svg > "public/assets/images/board/blue${i}x${j}.svg";done;done
-# generate background CSS
-for i in {1..12};do for j in {1..10}; do echo -e ".blue.board${i}x${j} cg-board {\n  background-image: url('images/board/blue${i}x${j}.svg');\n}";done;done > public/assets/theme-backgrounds.css
+for color in blue green brown;do
+    # generate SVGs
+    for i in {1..12};do for j in {1..10}; do sed "s#0 0 1200 1000#0 $(( 1000 - 100*j )) $(( 100*i )) $(( 100*j ))#" public/assets/images/board/${color}/${color}_base.svg > "public/assets/images/board/${color}/${color}${i}x${j}.svg";done;done
+    # generate background CSS
+    for i in {1..12};do for j in {1..10}; do echo -e ".${color}board.board${i}x${j} cg-board {\n  background-image: url('images/board/${color}/${color}${i}x${j}.svg');\n}";done;done > public/assets/theme-board-${color}board.css
+done
 
 # generate theme-letters CSS
-rm public/assets/theme-letters.css
+rm public/assets/theme-piece-letters.css
 for i in "white" "black";do
     for j in {a..z};do
         if [[ $i = "white" ]]
@@ -14,11 +16,13 @@ for i in "white" "black";do
         else
            letter=${j}
         fi
-        echo -e ".letters .cg-wrap piece.${j}-piece.${i} {background-image: url(\"data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' version='1.1' width='80px' height='80px'><text font-size='30' x='50%' y='50%' fill='${i}' class='$i' text-anchor='middle' dominant-baseline='central'>${letter}</text></svg>\");}" >> public/assets/theme-letters.css
+        echo -e ".letters .cg-wrap piece.${j}-piece.${i} {background-image: url(\"data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' version='1.1' width='80px' height='80px'><text font-size='30' x='50%' y='50%' fill='${i}' class='$i' text-anchor='middle' dominant-baseline='central'>${letter}</text></svg>\");}" >> public/assets/theme-piece-letters.css
         # promoted pieces
-        echo -e ".letters .cg-wrap piece.p${j}-piece.${i} {background-image: url(\"data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' version='1.1' width='80px' height='80px'><text font-size='30' x='50%' y='50%' fill='${i}' class='$i' text-anchor='middle' dominant-baseline='central'>+${letter}</text></svg>\");}" >> public/assets/theme-letters.css
+        echo -e ".letters .cg-wrap piece.p${j}-piece.${i} {background-image: url(\"data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' version='1.1' width='80px' height='80px'><text font-size='30' x='50%' y='50%' fill='${i}' class='$i' text-anchor='middle' dominant-baseline='central'>+${letter}</text></svg>\");}" >> public/assets/theme-piece-letters.css
     done
 done
+# walls
+echo -e ".letters .cg-wrap piece._-piece {background-image: url(\"data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' version='1.1' width='80px' height='80px'><text font-size='30' x='50%' y='50%' fill='red' class='red' text-anchor='middle' dominant-baseline='central'>✽</text></svg>\");}" >> public/assets/theme-piece-letters.css
 
 # generate CSS
 rm public/assets/generated.css
