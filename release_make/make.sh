51fbdaa2f7644e34efa4d364060d371fc84a88aa
yjf2002ghty <47345902+yjf2002ghty@users.noreply.github.com>
2023-12-23 16:40:00 +0800

    Update make.sh

--- release_make/make.sh
+++ release_make/make.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
 rm -rf ./release-builds
-mkdir -p ./release-builds/win
-mkdir -p ./release-builds/linux
+mkdir -p ./release-builds/win/x64
+mkdir -p ./release-builds/linux/x64
+mkdir -p ./release-builds/win/arm64
+mkdir -p ./release-builds/linux/arm64
 npm install -g pkg
-pkg . --target win --output ./release-builds/win/FairyGround.exe
-pkg . --target linux --output ./release-builds/linux/FairyGround
+pkg . --target win-x64 --output ./release-builds/win/x64/FairyGround.exe
+pkg . --target linux-x64 --output ./release-builds/linux/x64/FairyGround
+pkg . --target win-arm64 --output ./release-builds/win/arm64/FairyGround.exe
+pkg . --target linux-arm64 --output ./release-builds/linux/arm64/FairyGround
 cd ..
 npm run build
-cp -r ./public ./release_make/release-builds/win/
-cp -r ./public ./release_make/release-builds/linux/
+cp -r ./public ./release_make/release-builds/win/x64/
+cp -r ./public ./release_make/release-builds/linux/x64/
+cp -r ./public ./release_make/release-builds/win/arm64/
+cp -r ./public ./release_make/release-builds/linux/arm64/
 echo -e "Release build finished."
 pause
