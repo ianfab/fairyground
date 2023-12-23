e9e0be419c63a36a271eab80fd47ae82cfc3c81e
yjf2002ghty <47345902+yjf2002ghty@users.noreply.github.com>
2023-12-23 16:39:35 +0800

    Update make.bat

--- release_make/make.bat
+++ release_make/make.bat
@@ -5,13 +5,23 @@ rd /s /q .\release-builds
 md .\release-builds || goto error
 md .\release-builds\win || goto error
 md .\release-builds\linux || goto error
+md .\release-builds\win\x64 || goto error
+md .\release-builds\linux\x64 || goto error
+md .\release-builds\win\arm64 || goto error
+md .\release-builds\linux\arm64 || goto error
+
+::Platforms include x86, x64, arm, arm64
 (start /wait "" npm install -g pkg ^& exit) || goto error
-(start /wait "" pkg . --target win --output .\release-builds\win\FairyGround.exe ^& exit) || goto error
-(start /wait "" pkg . --target linux --output .\release-builds\linux\FairyGround ^& exit) || goto error
+(start /wait "" pkg . --target win-x64 --output .\release-builds\win\x64\FairyGround.exe ^& exit) || goto error
+(start /wait "" pkg . --target linux-x64 --output .\release-builds\linux\x64\FairyGround ^& exit) || goto error
+(start /wait "" pkg . --target win-arm64 --output .\release-builds\win\arm64\FairyGround.exe ^& exit) || goto error
+(start /wait "" pkg . --target linux-arm64 --output .\release-builds\linux\arm64\FairyGround ^& exit) || goto error
 cd ..
 (start /wait "" npm run buildwithcmd ^& exit) || goto error
-xcopy .\public .\release_make\release-builds\win\public /E /H /C /I || goto error
-xcopy .\public .\release_make\release-builds\linux\public /E /H /C /I || goto error
+xcopy .\public .\release_make\release-builds\win\x64\public /E /H /C /I || goto error
+xcopy .\public .\release_make\release-builds\linux\x64\public /E /H /C /I || goto error
+xcopy .\public .\release_make\release-builds\win\arm64\public /E /H /C /I || goto error
+xcopy .\public .\release_make\release-builds\linux\arm64\public /E /H /C /I || goto error
 echo Release build finished. Check "%~dp0release-builds\" to see the results.
 pause
 exit /b 0
