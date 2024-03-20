01a746d1bd2292e06e424ab0c1348fcdbdb291b3
yjf2002ghty <yjf2002ghty@outlook.com>
2024-03-20 13:48:04 +0800

    Script update

--- release_make/make.bat
+++ release_make/make.bat
@@ -1,5 +1,6 @@
 @echo off
 SETLOCAL ENABLEEXTENSIONS
+set nodeversion=node18
 cd /d "%~dp0"
 rd /s /q .\release-builds
 md .\release-builds || goto error
@@ -14,15 +15,23 @@ md .\release-builds\macos\x64 || goto error
 md .\release-builds\macos\arm64 || goto error
 
 ::Platforms include x86, x64, arm, arm64
-(start /wait "" npm install -g pkg ^& exit) || goto error
-(start /wait "" pkg . --target win-x64 --output .\release-builds\win\x64\FairyGround.exe ^& exit) || goto error
-(start /wait "" pkg . --target linux-x64 --output .\release-builds\linux\x64\FairyGround ^& exit) || goto error
-(start /wait "" pkg . --target win-arm64 --output .\release-builds\win\arm64\FairyGround.exe ^& exit) || goto error
-(start /wait "" pkg . --target linux-arm64 --output .\release-builds\linux\arm64\FairyGround ^& exit) || goto error
-(start /wait "" pkg . --target macos-x64 --output .\release-builds\macos\x64\FairyGround ^& exit) || goto error
-(start /wait "" pkg . --target macos-arm64 --output .\release-builds\macos\arm64\FairyGround ^& exit) || goto error
+(start /wait "" npm install -g pkg ^&^& exit) || goto error
+
+call :Make %nodeversion%-win-x64 .\release-builds\win\x64\FairyGround.exe
+if "%errorlevel%"=="11" (goto error)
+call :Make %nodeversion%-linux-x64 .\release-builds\linux\x64\FairyGround
+if "%errorlevel%"=="11" (goto error)
+call :Make %nodeversion%-win-arm64 .\release-builds\win\arm64\FairyGround.exe
+if "%errorlevel%"=="11" (goto error)
+call :Make %nodeversion%-linux-arm64 .\release-builds\linux\arm64\FairyGround
+if "%errorlevel%"=="11" (goto error)
+call :Make %nodeversion%-macos-x64 .\release-builds\macos\x64\FairyGround.app
+if "%errorlevel%"=="11" (goto error)
+call :Make %nodeversion%-macos-arm64 .\release-builds\macos\arm64\FairyGround.app
+if "%errorlevel%"=="11" (goto error)
+
 cd ..
-(start /wait "" npm run buildwithcmd ^& exit) || goto error
+(start /wait "" npm run buildwithcmd ^&^& exit) || goto error
 xcopy .\public .\release_make\release-builds\win\x64\public /E /H /C /I /Q || goto error
 xcopy .\public .\release_make\release-builds\linux\x64\public /E /H /C /I /Q || goto error
 xcopy .\public .\release_make\release-builds\win\arm64\public /E /H /C /I /Q || goto error
@@ -37,3 +46,34 @@ exit /b 0
 echo Release build failed.
 pause
 exit /b 1
+
+:Make
+start /WAIT "" cmd.exe /C ^(pkg . --target %~1 --output %2 ^& exit ^) ^> %TEMP%\make_fairyground.log 2^>^&1
+set result=
+FOR /F "usebackq" %%i IN (`findstr /L /I "Error" "%TEMP%\make_fairyground.log"`) DO set result=%%i
+if not "%result%"=="" (
+    echo Error: Build failed. Check the log below to see what's going on. File: %TEMP%\make_fairyground.log
+    type %TEMP%\make_fairyground.log
+    exit /b 11
+)
+set result=
+FOR /F "usebackq" %%i IN (`findstr /L /I "Failed to make bytecode" "%TEMP%\make_fairyground.log"`) DO set result=%%i
+if not "%result%"=="" (
+    echo Fail： Bytecode generation failed. Trying --no-bytecode...
+    call :TryNoByteCode %~1 %2
+    if "%errorlevel%"=="11" (exit /b 11)
+)
+echo Pass: %~1
+exit /b 0
+
+:TryNoByteCode
+set PATH=%PATH%;%~dp0ldid\win_x64
+start /WAIT "" cmd.exe /C ^(pkg . --no-bytecode --public --public-packages --target %~1 --output %2 ^& exit ^)  ^> %TEMP%\make_fairyground.log 2^>^&1
+set result=
+FOR /F "usebackq" %%i IN (`findstr /L /I "Error" "%TEMP%\make_fairyground.log"`) DO set result=%%i
+if not "%result%"=="" (
+    echo Error: Build failed. Check the log below to see what's going on. File: %TEMP%\make_fairyground.log
+    type %TEMP%\make_fairyground.log
+    exit /b 11
+)
+exit /b 0
--- release_make/make.sh
+++ release_make/make.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+function Error() {
+    echo Release build failed.
+    echo "Press any key to continue..."
+    read -n 1
+    echo
+    exit 1
+}
+
+export nodeversion="node18"
+
+rm -rf ./ldid
+git clone https://github.com/daeken/ldid.git || Error
+cd ldid
+./make.sh
+cd ..
+export PATH="$PATH:$(pwd)/ldid"
+
 rm -rf ./release-builds
 mkdir -p ./release-builds/win/x64
 mkdir -p ./release-builds/linux/x64
@@ -7,13 +24,51 @@ mkdir -p ./release-builds/win/arm64
 mkdir -p ./release-builds/linux/arm64
 mkdir -p ./release-builds/macos/x64
 mkdir -p ./release-builds/macos/arm64
+
 npm install -g pkg
-pkg . --target win-x64 --output ./release-builds/win/x64/FairyGround.exe
-pkg . --target linux-x64 --output ./release-builds/linux/x64/FairyGround
-pkg . --target win-arm64 --output ./release-builds/win/arm64/FairyGround.exe
-pkg . --target linux-arm64 --output ./release-builds/linux/arm64/FairyGround
-pkg . --target macos-x64 --output ./release-builds/macos/x64/FairyGround
-pkg . --target macos-arm64 --output ./release-builds/macos/arm64/FairyGround
+
+function TryNoByteCode() {
+    pkg . --no-bytecode --public --public-packages --target $1 --output $2 >/tmp/make_fairyground.log 2>&1
+    if [ "$(grep 'Error' /tmp/make_fairyground.log)" != "" ]; then
+        echo Error: Build failed. Check the log below to see what\'s going on. File: /tmp/make_fairyground.log
+        cat /tmp/make_fairyground.log
+        return 11
+    fi
+    return 0
+}
+
+function Make() {
+    pkg . --target $1 --output $2 >/tmp/make_fairyground.log 2>&1
+    if [ "$(grep 'Error' /tmp/make_fairyground.log)" != "" ]; then
+        echo Error: Build failed. Check the log below to see what\'s going on. File: /tmp/make_fairyground.log
+        cat /tmp/make_fairyground.log
+        return 11
+    fi
+    if [ "$(grep 'Failed to make bytecode' /tmp/make_fairyground.log)" != "" ]; then
+        echo "Fail: Bytecode generation failed. Trying --no-bytecode..."
+        TryNoByteCode $1 $2
+        if [ $? -eq 11 ]; then
+            return 11
+        fi
+    fi
+    echo "Pass: $1"
+    return 0
+}
+
+
+Make "$nodeversion"-win-x64 ./release-builds/win/x64/FairyGround.exe
+if [ $? -eq 11 ]; then Error; fi
+Make "$nodeversion"-linux-x64 ./release-builds/linux/x64/FairyGround
+if [ $? -eq 11 ]; then Error; fi
+Make "$nodeversion"-win-arm64 ./release-builds/win/arm64/FairyGround.exe
+if [ $? -eq 11 ]; then Error; fi
+Make "$nodeversion"-linux-arm64 ./release-builds/linux/arm64/FairyGround
+if [ $? -eq 11 ]; then Error; fi
+Make "$nodeversion"-macos-x64 ./release-builds/macos/x64/FairyGround.app
+if [ $? -eq 11 ]; then Error; fi
+Make "$nodeversion"-macos-arm64 ./release-builds/macos/arm64/FairyGround.app
+if [ $? -eq 11 ]; then Error; fi
+
 cd ..
 npm run build
 cp -r ./public ./release_make/release-builds/win/x64/
@@ -23,4 +78,7 @@ cp -r ./public ./release_make/release-builds/linux/arm64/
 cp -r ./public ./release_make/release-builds/macos/x64/
 cp -r ./public ./release_make/release-builds/macos/arm64/
 echo -e "Release build finished."
-pause
+echo "Press any key to continue..."
+read -n 1
+echo
+exit 0
