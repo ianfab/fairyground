bb5214135df739df4c0400b6413847512b77571f
yjf2002ghty <47345902+yjf2002ghty@users.noreply.github.com>
2024-11-07 14:03:27 +0800

    Fix CI

--- release_make/CI.sh
+++ release_make/CI.sh
@@ -125,9 +125,6 @@ npm run build || Error
 rm -f ./public/vercel.json
 find ./public -type f -name "original.*" -exec rm -f {} \;
 
-cat ./src/js/server.js > ./release_make/index.js
-cat ./release_make/tail.js > ./release_make/index.js
-
 cp -r ./public ./release_make/release-builds/win/x64/
 cp -r ./public ./release_make/release-builds/linux/x64/
 cp -r ./public ./release_make/release-builds/win/arm64/
